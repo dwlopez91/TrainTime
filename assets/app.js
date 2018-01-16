6440968b9d5e8795fcc13c9e27e2458896a31f3d
David Lopez <dwlopez91@gmail.com>
2018-01-16 00:06:59 -0600

    second

---
+++ assets/app.js
@@ -0,0 +1,9 @@
+  var config = {
+    apiKey: "AIzaSyBawuQ2xPZica8Yl4y-lJ-TBLcqFoEsvP4",
+    authDomain: "train-times-a9676.firebaseapp.com",
+    databaseURL: "https://train-times-a9676.firebaseio.com",
+    projectId: "train-times-a9676",
+    storageBucket: "",
+    messagingSenderId: "167523479862"
+  };
+  firebase.initializeApp(config);
